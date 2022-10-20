8558a0edd778b2374f4ae9451e83a4fb2ae07be8
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-10-20 08:37:07 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var map = L.map('map', {attributionControl: false}).setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([51.5, -0.09]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
