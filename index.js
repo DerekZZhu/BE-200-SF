4c3608a4abcc078dadef1690741237f40698319f
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-10-20 22:35:12 -0700

    Shifted space to LA

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var map = L.map('map', {attributionControl: false}).setView([51.505, -0.09], 13);
+var map = L.map('map', {attributionControl: false}).setView([37.78755, -122.40661], 15);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([51.5, -0.09]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+var marker = L.marker([37.78755, -122.40661]).addTo(map);
+L.marker([51.9, -0.1]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").bindPopup();
 
